2639cb9cb15ad6f849a208310e10a6760bc6a9b0
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-12-12 18:48:00 +0530

    generating api key on TMDB and compleated responsive nav

--- JavaScript/script1.js
+++ JavaScript/script1.js
@@ -1,3 +1,5 @@
+// Script for open side menu and hide menu when you click on Icon 
+
 var sideOption=document.getElementById("side");
 function showMenu(){
     sideOption.style.left="0";
@@ -5,3 +7,20 @@ function showMenu(){
 function hideMenu(){
     sideOption.style.left="-300px";
 }
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '144cee50bfmshb43713f1c85811ep164433jsn972d85b733e7',
+// 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+// 	.then(response => response.json())
+//     .then(data => {const list = data.d})
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+//     console.log(list)
